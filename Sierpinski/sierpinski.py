6aaed515f836e3ab37130aa31ac6c502747a4dce
Luispapiernik <lpapiernik24@gmail.com>
2018-01-04 14:41:21 -0500

    Update sierpinski.py

--- Sierpinski/sierpinski.py
+++ Sierpinski/sierpinski.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from PIL import Image
 
-# colores
+# colors
 COLORS = {'WHITE': (255, 255, 255), 'BLACK': (0, 0, 0), 'CYAN': (0, 255, 255),
           'GREEN': (0, 255, 0), 'BLUE': (0, 0, 255), 'YELLOW': (255, 255, 0),
           'ORANGE': (255, 165, 0), 'MAGENTA': (255, 0, 255),
